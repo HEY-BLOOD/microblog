89299d6c2c52835a5cf1cbba7acd5bd7b5d7d0c9
Blood Hwang <blood1740@aliyun.com>
2020-08-29 00:06:46 +0800

    模板

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from app import app  # 从app包导入其成员app变量
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [{
+        'author': {
+            'username': 'John'
+        },
+        'body': 'Beautiful day in Portland!'
+    }, {
+        'author': {
+            'username': 'Susan'
+        },
+        'body': 'The Avengers movie was so cool!'
+    }]
+    return render_template('index.html', title='Home', user=user, posts=posts)
