58bba5a5947bc2ef7f6f6490dfb4dc4a9c2d81af
Blood Hwang <blood1740@aliyun.com>
2020-08-29 00:34:12 +0800

    Web表单

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
 from app import app  # 从app包导入其成员app变量
-from flask import render_template
 
 
 @app.route('/')
@@ -18,3 +19,13 @@ def index():
         'body': 'The Avengers movie was so cool!'
     }]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # 验证表单数据
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
