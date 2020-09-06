fc5114ab04c846595c0b2ad596830a0babb9b506
Blood Hwang <blood1740@aliyun.com>
2020-09-06 17:41:19 +0800

    用户登录

--- app/models.py
+++ app/models.py
@@ -1,14 +1,22 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-from app import db
+from app import db, login
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     # db.relationship()，“多的”类为Post，backref反向调用的属性名author，lazy执行方式
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -23,3 +31,9 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    """ 用户加载函数 """
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,16 @@
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask import render_template, flash, redirect, url_for
-from app import app  # 从app包导入其成员app变量
+from app import app, db  # 从app包导入其成员app变量
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from flask import request
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [{
         'author': {
             'username': 'John'
@@ -18,14 +22,52 @@ def index():
         },
         'body': 'The Avengers movie was so cool!'
     }]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():  # 验证表单数据
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    # 用户是否通过登录认证
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    # 验证表单数据
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        # 用户名或密码有误
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # 注册用户为已登录
+        login_user(user, remember=form.remember_me.data)
+        # 登录后页面重定向
+        next_page = request.args.get('next')
+        # 检查重定向与应用保持在同一站点，使用应用更安全
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """ 登出用户 """
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # 用户是否通过登录认证
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    # 验证表单数据
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
